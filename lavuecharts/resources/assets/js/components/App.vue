1b8c62aace986b4fe1a88575e1efc1caf06a015d
VANKOCKO <van.kocko@gmail.com>
2018-03-21 04:25:03 +0100

    test

---
+++ lavuecharts/resources/assets/js/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1></h1>
+
+        <p>
+            <router-link :to="{ name: 'coin' }">coin</router-link> |
+        </p>
+
+        <div class="container">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+<script>
+    export default {}
+</script>
